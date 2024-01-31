c24cf507ba06af2e6c31b4aa277ad527cfa57942
lbertoncello <lucas_b_andrade@hotmail.com>
2024-01-31 15:31:09 -0300

    chore: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config"],
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['@nuxt/eslint-config', 'plugin:prettier/recommended'],
 }
