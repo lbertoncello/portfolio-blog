911a3edd51df86df01547192ce41a93aac029be5
lbertoncello <lucas_b_andrade@hotmail.com>
2024-02-01 12:51:19 -0300

    feat: add projects page to navbar

--- components/navbar-menu.vue
+++ components/navbar-menu.vue
@@ -2,10 +2,13 @@
   <nav class="font-mono">
     <ul class="flex space-x-4">
       <li>
-        <NuxtLink to="/" class="link"> Main </NuxtLink>
+        <NuxtLink to="/" class="link">Main</NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/about" class="link"> About </NuxtLink>
+        <NuxtLink to="/about" class="link">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/projects" class="link">Projects</NuxtLink>
       </li>
     </ul>
   </nav>
