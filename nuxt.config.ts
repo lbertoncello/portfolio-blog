6fb49737eba5620202f74018f9ab959b8162d574
lbertoncello <lucas_b_andrade@hotmail.com>
2024-02-01 18:24:56 -0300

    feat: add highlight to code content within blob posts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,14 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxt/content',
   ],
+  content: {
+    highlight: {
+      theme: {
+        default: 'min-light',
+        dark: 'min-dark',
+      },
+    },
+  },
   // To make Color Mode compatible with TailWind
   colorMode: {
     classSuffix: '',
