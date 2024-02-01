a563dbb6796b7666f77823487929184d0901ffe7
lbertoncello <lucas_b_andrade@hotmail.com>
2024-02-01 19:56:10 -0300

    feat: add listing of all blog posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>All blog posts.</div>
+  <section>
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script setup>
+const { data: posts } = await useAsyncData('blog-lists', () =>
+  queryContent('/blog').only(['title', '_path']).find()
+)
+
+// Meta data
+useSeoMeta({
+  title: 'Blog List',
+  description: 'This is the list of all blog posts',
+  ogTitle: 'Blog List',
+  twitterTitle: 'Blog List',
+})
+</script>
