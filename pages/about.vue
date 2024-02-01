91e544e051e5c3b8868d42ddce52854dcdfdc98f
lbertoncello <lucas_b_andrade@hotmail.com>
2024-02-01 18:04:04 -0300

    feat: add add content to the about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,8 @@
 <template>
-  <div>This page is about me.</div>
+  <article>
+    <ContentDoc
+      path="/about"
+      class="prose dark:prose-invert prose-h1:text-2xl"
+    />
+  </article>
 </template>
-
-<script setup>
-useSeoMeta({
-  title: 'About',
-  description: 'This is the About page',
-  ogTitle: 'About Page',
-  twitterTitle: 'About Page',
-})
-</script>
