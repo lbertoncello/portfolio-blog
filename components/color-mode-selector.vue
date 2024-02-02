16516fc038e456ce4f02d7dc8fe9861a6f029df9
lbertoncello <lucas_b_andrade@hotmail.com>
2024-02-02 19:28:43 -0300

    feat: improve pages responsiveness

--- components/color-mode-selector.vue
+++ components/color-mode-selector.vue
@@ -4,7 +4,7 @@
       Change to {{ nextMode }}
     </div>
     <button
-      class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500"
+      class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500 text-4xl md:text-base"
       @click="toggleMode"
       @mouseenter="showNextModelLabel = true"
       @mouseleave="showNextModelLabel = false"
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -9,10 +9,15 @@
       </template>
       <template #default="{ doc }">
         <div class="grid grid-cols-6 gap-16">
-          <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+          <div
+            :class="{
+              'col-span-6 md:col-span-4': doc.toc,
+              'col-span-6': !doc.toc,
+            }"
+          >
             <ContentRenderer :value="doc" />
           </div>
-          <div v-if="doc.toc" class="col-span-2 not-prose">
+          <div v-if="doc.toc" class="hidden md:block col-span-2 not-prose">
             <aside class="sticky top-8">
               <div class="font-semibold mb-2">Table of Contents</div>
               <nav>
