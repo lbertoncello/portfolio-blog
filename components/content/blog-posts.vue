37cdcb96578667cc10b1eb0990c0b73548772af8
lbertoncello <lucas_b_andrade@hotmail.com>
2024-02-02 19:00:20 -0300

    feat: add index page content

--- components/content/blog-posts.vue
+++ components/content/blog-posts.vue
@@ -1,38 +1,50 @@
 <template>
-  <section class="not-prose font-mono">
-    <div class="column text-gray-400 text-sm">
-      <div>date</div>
-      <div>title</div>
-    </div>
-    <ul>
-      <li v-for="post in posts" :key="post._path">
-        <NuxtLink
-          :to="post._path"
-          class="column hover:bg-gray-100 dark:hover:bg-gray-800"
-        >
-          <div
-            :class="{
-              invisible: !post.displayYear,
-              'text-gray-400 dark:text-gray-500': post.displayYear,
-            }"
+  <slot :posts="posts">
+    <section class="not-prose font-mono">
+      <div class="column text-gray-400 text-sm">
+        <div>date</div>
+        <div>title</div>
+      </div>
+      <ul>
+        <li v-for="post in posts" :key="post._path">
+          <NuxtLink
+            :to="post._path"
+            class="column hover:bg-gray-100 dark:hover:bg-gray-800"
           >
-            {{ post.year }}
-          </div>
-          <div>{{ post.title }}</div>
-        </NuxtLink>
-      </li>
-    </ul>
-  </section>
+            <div
+              :class="{
+                invisible: !post.displayYear,
+                'text-gray-400 dark:text-gray-500': post.displayYear,
+              }"
+            >
+              {{ post.year }}
+            </div>
+            <div>{{ post.title }}</div>
+          </NuxtLink>
+        </li>
+      </ul>
+    </section>
+  </slot>
 </template>
 
 <script setup>
-const { data } = await useAsyncData('blog-lists', () =>
-  queryContent('/blog')
+const props = defineProps({
+  limit: {
+    type: Number,
+    default: null,
+  },
+})
+
+const { data } = await useAsyncData('blog-lists', () => {
+  const query = queryContent('/blog')
     .where({ _path: { $ne: '/blog' } })
     .only(['title', '_path', 'publishedAt'])
     .sort({ publishedAt: -1 })
-    .find()
-)
+
+  if (props.limit) query.limit(props.limit)
+
+  return query.find()
+})
 
 const posts = computed(() => {
   if (!data.value) return []
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,5 @@
 <template>
-  <div>Hello from the main page!</div>
+  <article class="prose dark:prose-invert max-w-none">
+    <ContentDoc path="/" />
+  </article>
 </template>
-
-<script setup>
-useSeoMeta({
-  title: 'Main',
-  description: 'This is the Main page',
-  ogTitle: 'Main Page',
-  twitterTitle: 'Main Page',
-})
-</script>
