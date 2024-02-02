9e76e0e013c11972b61ce011442231695960f148
lbertoncello <lucas_b_andrade@hotmail.com>
2024-02-02 17:07:46 -0300

    feat: add error page for blog post not found

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,20 +2,29 @@
   <article
     class="prose dark:prose-invert prose-pre:bg-gray-50 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none"
   >
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-16">
-        <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
-          <ContentRenderer :value="doc" />
+    <ContentDoc>
+      <template #not-found>
+        <h1>Document not found (404)</h1>
+        <p>This blog post could not be found.</p>
+      </template>
+      <template #default="{ doc }">
+        <div class="grid grid-cols-6 gap-16">
+          <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+            <ContentRenderer :value="doc" />
+          </div>
+          <div v-if="doc.toc" class="col-span-2 not-prose">
+            <aside class="sticky top-8">
+              <div class="font-semibold mb-2">Table of Contents</div>
+              <nav>
+                <TocLinks
+                  :links="doc.body?.toc?.links"
+                  :active-id="activedId"
+                />
+              </nav>
+            </aside>
+          </div>
         </div>
-        <div v-if="doc.toc" class="col-span-2 not-prose">
-          <aside class="sticky top-8">
-            <div class="font-semibold mb-2">Table of Contents</div>
-            <nav>
-              <TocLinks :links="doc.body?.toc?.links" :active-id="activedId" />
-            </nav>
-          </aside>
-        </div>
-      </div>
+      </template>
     </ContentDoc>
   </article>
 </template>
